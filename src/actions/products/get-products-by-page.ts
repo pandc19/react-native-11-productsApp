af92548438578d237a3ece671d90fbb44cbf72f6
Jonathan Pillajo <pandc19@outlook.com>
2025-07-06 18:34:22 -0500

    feat: implement infinite scrolling and pull-to-refresh functionality in ProductList and HomeScreen components

--- src/actions/products/get-products-by-page.ts
+++ src/actions/products/get-products-by-page.ts
@@ -3,14 +3,14 @@ import {TesloProduct} from '../../infrastructure/interfaces/teslo-products.respo
 import {ProductMapper} from '../../infrastructure/mappers/product.mapper';
 
 export const getProductsByPage = async (page: number, limit: number = 20) => {
-//   console.log({page, limit});
+  //   console.log({page, limit});
   try {
     const {data} = await tesloApi.get<TesloProduct[]>(
       `/products?limit=${limit}&offset=${page * 10}`,
     );
 
     const products = data.map(ProductMapper.tesloProductoToEntity);
-
+    // console.log({products});
     return products;
   } catch (error) {
     console.log(error);
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,22 +1,40 @@
-import {useQuery} from '@tanstack/react-query';
+import {useInfiniteQuery, useQuery} from '@tanstack/react-query';
 import {getProductsByPage} from '../../../actions/products/get-products-by-page';
 import {MainLayout} from '../../layouts/MainLayout';
 import {FullScreenLoader} from '../../components/ui/FullScreenLoader';
-import {Text} from '@ui-kitten/components';
 import {ProductList} from '../../components/products/ProducList';
 
 export const HomeScreen = () => {
-  const {isLoading, data: products = []} = useQuery({
+  // const {isLoading, data: products = []} = useQuery({
+  //   queryKey: ['products', 'infinite'],
+  //   staleTime: 1000 * 60 * 60,
+  //   queryFn: () => getProductsByPage(0),
+  // });
+
+  const {isLoading, data, fetchNextPage} = useInfiniteQuery({
     queryKey: ['products', 'infinite'],
     staleTime: 1000 * 60 * 60,
-    queryFn: () => getProductsByPage(0),
+    initialPageParam: 0,
+    queryFn: async params => {
+      console.log({params});
+      const products = await getProductsByPage(params.pageParam);
+      return products;
+    },
+    getNextPageParam: (lastPage, allPages) => allPages.length,
   });
 
   return (
     <MainLayout
       title="TesloShop - Products"
       subtitle="Aplicación administrativa">
-      {isLoading ? <FullScreenLoader /> : <ProductList products={products} />}
+      {isLoading ? (
+        <FullScreenLoader />
+      ) : (
+        <ProductList
+          products={data?.pages.flat() ?? []}
+          fetchNextPage={fetchNextPage}
+        />
+      )}
     </MainLayout>
   );
 };
